921d95d94bfd97d7aef9a609384e7fcdf2c86f93
Bruno-Gomes-QA <bruno.gomes.qa@gmail.com>
2022-11-11 21:24:56 -0300

    vercel port config

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 8080;
 
+app.use(express.json({ extended: false }));
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 app.use('/', require('./routes/opniaoRoutes'));
-
-app.listen(3000);
 
