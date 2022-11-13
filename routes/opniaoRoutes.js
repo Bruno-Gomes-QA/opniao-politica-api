9a1b250bea39cb3c7fa5cfebb431ce237a5f6987
Bruno-Gomes-QA <bruno.gomes.qa@gmail.com>
2022-11-13 10:28:16 -0300

    aws-test

--- routes/opniaoRoutes.js
+++ routes/opniaoRoutes.js
@@ -3,43 +3,31 @@ const router = express.Router();
 
 async function ValidateCpf(cpf) {
 
-    let puppeteer
-    let chromium
-    let options
-    let browser
+    const chromium = require("chrome-aws-lambda");
+    const puppeteer = require("puppeteer-core");
+    const options = {
+        args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chromium.defaultViewport,  
+        executablePath: await chromium.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+    };
+    browser = await chromium.puppeteer.launch(options);
 
-    if (process.env.AWS_LAMBDA) {
-        chromium = require("chrome-aws-lambda");
-        puppeteer = require("puppeteer-core");
-        options = {
-            args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-            defaultViewport: chromium.defaultViewport,  
-            executablePath: await chromium.executablePath,
-            headless: true,
-            ignoreHTTPSErrors: true,
-        };
-        browser = await chromium.puppeteer.launch(options);
-    } else {
-        puppeteer = require("puppeteer");
-        browser = await puppeteer.launch();
-    }
-
-  console.log('init - 3')
-  const page = await browser.newPage();
-  console.log('google open')
-  const inputCpf = '#SE_NomeTituloCPF'
-  const result = '#return-form-situacao-eleitoral > p:nth-child(2)'
-  
-  await page.goto('https://www.tse.jus.br/eleitor/titulo-e-local-de-votacao/copy_of_consulta-por-nome');
-  await page.waitForSelector(inputCpf)
-  await page.type(inputCpf, cpf, {delay:100})
-  await page.waitForSelector(inputCpf)
-  await page.keyboard.press('Enter')
-  await page.waitForSelector(result)
-  const situacao = await page.evaluate(() => {
-      return document.querySelector('#return-form-situacao-eleitoral > p:nth-child(2)').textContent
-  })
-  console.log(`consulta realizada${situacao}`)
+    const page = await browser.newPage();
+    const inputCpf = '#SE_NomeTituloCPF'
+    const result = '#return-form-situacao-eleitoral > p:nth-child(2)'
+    
+    await page.goto('https://www.tse.jus.br/eleitor/titulo-e-local-de-votacao/copy_of_consulta-por-nome');
+    await page.waitForSelector(inputCpf)
+    await page.type(inputCpf, cpf, {delay:100})
+    await page.waitForSelector(inputCpf)
+    await page.keyboard.press('Enter')
+    await page.waitForSelector(result)
+    const situacao = await page.evaluate(() => {
+        return document.querySelector('#return-form-situacao-eleitoral > p:nth-child(2)').textContent
+    })
+    
     await browser.close()
     return situacao
     
